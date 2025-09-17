41c4f2c5a5001dfc6869401c89685143cc256e60
Sam Lapp <sammlapp@gmail.com>
2025-09-17 16:34:02 -0400

    improve text in install script

--- script/install.sh
+++ script/install.sh
@@ -8,11 +8,13 @@ echo "=> Installing picopy...\n"
 sudo cp picopy.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/picopy.py
 
-echo "=> Starting picopy...\n"
 sudo cp picopy.sh /etc/init.d/
 sudo chmod +x /etc/init.d/picopy.sh
 
 sudo update-rc.d picopy.sh defaults
-sudo /etc/init.d/picopy.sh start
 
 echo "picopy installed.\n"
+
+echo "=> Starting picopy...\n"
+sudo /etc/init.d/picopy.sh start
+
